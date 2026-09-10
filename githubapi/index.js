7eb35c0181fffdc70451705d5a6b16ccdfd5bcd0
Nauter <Kaur.Pakaste@gmail.com>
2026-09-10 15:29:04 +0300

    Add files via upload

--- githubapi/index.js
+++ githubapi/index.js
@@ -1,41 +1,40 @@
-let givenProfile="";
-let profileName = "";
-let profileId = "";
-let profileLink = "";
-let profileRepos = "";
+let givenProfile=""
+let profileName = ""
+let profileId = ""
+let profileLink = ""
+let profileRepos = ""
 
-function renderPage(){
-    document.getElementById("app").innerHTML = `
-    <div>
-    <h1>Github Profile Viewer</h1>
-    <p>Please enter profile name: </p>
-    <input />
-     <div class="content">
-     <h1 id="name">Name: ${profileName}</h1>
-     <p id="id">Id: ${profileid}</p>
-     <p id="repos">Public repos: ${profileRepos}</p>
-     <p id="profileurl">Link: ${profileLink}<a href="${profileName}" target="_blank">/users/</a></p>
-     </div>
-    </div>
-    `;
-}
-
-renderPage();
-
-const input = document.querySelector("input");
-input.addEventListener("change",updateValue);
-
-function updateValue(e){
-    givenProfile = e.target.value;
-    fetchProfile();
-}
-
-let fetchProfile = async () => {
+let fetchProfile = async (personalToken) => {
     let fetchedData;
-
-    await fetch(`https://api.github.com/users/${}`).then((response) => response.json()).then((data) => (fetchedData = data));
-
+    const headers = new Headers()
+    headers.set("Authorization",personalToken)
+    
+    await fetch(`https://api.github.com/users/${givenProfile}`,{
+        headers
+    }).then((response) => response.json()).then((data) => (fetchedData = data));
     profileName = fetchedData.login;
     profileId = fetchedData.id;
-    profileLink = fetchedData.node_id;
+    profileRepos = fetchedData.public_repos;
+    profileLink = fetchedData.url;
+    renderContent();
 }
+
+const input = document.querySelector("input")
+input.addEventListener("change", updateContent)
+
+function updateContent(e){
+    const personalToken = prompt("insert your github personal token :P")
+    givenProfile = e.target.value
+    fetchProfile(personalToken)
+}
+
+function renderContent(){
+    document.getElementById("content").innerHTML = `
+     <h2 id="name">Name: ${profileName}</h1>
+     <p id="id">Id: ${profileId}</p>
+     <p id="repos">Public repos: ${profileRepos}</p>
+     <p id="profileurl">Link:<a href="${profileName}" target="_blank">/users/</a></p>
+    `
+}
+
+renderContent();
